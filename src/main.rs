2147e8293666ff0acf7cbab8d13bbdff404ba4b8
Charles Bine <charles.bine@gmail.com>
2023-11-19 21:23:05 -0500

    add CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
+use clap::Parser;
+
 mod board;
 mod evaluate;
 mod search;
 mod game;
 
+
 use crate::game::*;
 
 
-fn run() {
-    let mut game = Game::new(4);
-    game.play(20);
-    println!("{}", game.to_pgn());
+/// Run a chess game of the bot against itself
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// number of moves deep to search
+    #[arg(short, long, default_value_t = 4)]
+    depth: u8,
+
+    /// Number of moves to play
+    #[arg(short, long, default_value_t = 20)]
+    count: u32,
 }
 
+
 fn main() {
-    run();
+    let args = Args::parse();
+    let mut game = Game::new(args.depth);
+    game.play(args.count as i32);
+    println!("{}", game.to_pgn());
 }
